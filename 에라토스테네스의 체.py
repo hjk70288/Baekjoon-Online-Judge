7f94157ea0f256652e732fb00022446beb0240d6
Yongmin Kim <56548968+hjk70288@users.noreply.github.com>
2019-11-18 15:14:52 +0900

    Update 에라토스테네스의 체.py

--- 에라토스테네스의 체.py
+++ 에라토스테네스의 체.py
@@ -1,10 +1,7 @@
-def isPrime(n):
-    for i in range(n*2,1000001,n):
-        yield i
-arr=[1]*1000001
-arr[1]=0
-for i in range(2,1000001):
-    if arr[i]==0:
-        continue
-    for j in isPrime(i):
-        arr[j]=0
+def prime_list(n):   #n까지의 소수 배열 반환 (소수면 True)
+    sieve = [True]*(n+1)  #에라토스테네스의 체 초기화
+    for i in range(2,n+1):  #2부터 입력한 수까지 반복
+        if sieve[i] == True: #검사한 수가 소수면      
+            for j in range(i+i, n, i): #그 수의 배수들을 False로 입력
+                sieve[j] = False
+    return sieve
